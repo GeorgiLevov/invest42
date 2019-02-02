8faed133f4c75fc90da52addcf35e8965388d3cd
georgelevov <georgelevov@gmail.com>
2019-02-02 14:31:32 +0200

    client-management-update

--- client/src/app/manager/management/manage-client/manage-client.component.ts
+++ client/src/app/manager/management/manage-client/manage-client.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ManageClientComponent implements OnInit {
 
+  public managing = true;
   public navMode = 'Management Mode';
   public navMenu = [
     {
