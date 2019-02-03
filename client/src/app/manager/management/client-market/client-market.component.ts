26a12554f9f27ff88adaff7fbec5d399b0f91a5c
georgelevov <georgelevov@gmail.com>
2019-02-03 14:40:25 +0200

    design-update

--- client/src/app/manager/management/client-market/client-market.component.ts
+++ client/src/app/manager/management/client-market/client-market.component.ts
@@ -13,7 +13,7 @@ import { BuyOrderComponent } from '../../manager-modals/buy-modal/buy-order.comp
 })
 export class ClientMarketComponent implements OnInit, AfterViewInit {
 
-  displayedColumns: string[] = ['name', 'industry', 'startprice', 'currentprice', 'addToWatchlsit', 'buy', 'sell'];
+  displayedColumns: string[] = ['name', 'industry', 'highprice', 'currentprice', 'buy', 'addToWatchlsit',];
   dataSource = new MatTableDataSource<any>();
   index: number;
   id: number;
@@ -40,13 +40,16 @@ export class ClientMarketComponent implements OnInit, AfterViewInit {
       .subscribe((res) => {
         this.dataSource.data = res;
 
-          setInterval((): any => {
-            (this.dataSource.data).forEach((company) => {
-              const direction = (Math.random() >= 0.5) ? 1 : -1;
-              const priceToUpdate = Math.round((direction * Math.random()) + company.currentprice );
-              company.currentprice = priceToUpdate;
-            });
-           }, 500);
+        setInterval((): any => {
+          (this.dataSource.data).forEach((company) => {
+            const direction = (Math.random() >= 0.5) ? 1 : -1;
+            let priceToUpdate = ((direction * Math.random()) + company.currentprice);
+            priceToUpdate = Number(priceToUpdate).toFixed(2);
+            if (priceToUpdate >= company.lowprice && priceToUpdate <= company.highprice ) {
+              company.currentprice = Number(priceToUpdate);
+            }
+          });
+        }, 1000);
       });
   }
 
